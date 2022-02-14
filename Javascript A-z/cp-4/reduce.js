2f8f1e70ff5c8df7122ea5ca6ac2061c72d0f7ec
shakilmahammud <hshakil987@gmail.com>
2022-02-14 15:24:28 +0600

    js string

--- Javascript A-z/cp-4/reduce.js
+++ Javascript A-z/cp-4/reduce.js
@@ -1,2 +1,18 @@
-// start reduce with loop
-// reduce example practices
+
+//string in javascript
+
+let n =10;
+let str = String(n);
+// let str = new String(n); not use new keyword convert string because it's give a object 
+//console.log(srt); return string n value
+
+//** * Escape Notation
+
+let str_s = 'hello This is a `\nstring`'
+console.log(str_s)
+
+// ========= String Comparison
+// js string comparison use  Lexico Graphic System to Compare To String
+let a = 'abc';
+let b = 'bcd';
+console.log(a<b) //it's return true
