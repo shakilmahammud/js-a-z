b8096cfd68815430c56c4c200d49075f34a52386
shakilmahammud <hshakil987@gmail.com>
2022-02-15 22:33:57 +0600

    string pratices

---
+++ Javascript A-z/cp-4/string.js
@@ -0,0 +1,32 @@
+
+//string in javascript
+
+let n =10;
+let str = String(n);
+// let str = new String(n); not use new keyword convert string because it's give a object 
+//console.log(srt); return string n value
+
+//** * Escape Notation
+
+let str_s = 'hello This is a `\nstring`'
+console.log(str_s)
+
+// ========= String Comparison
+// js string comparison use  Lexico Graphic System to Compare To String
+var a = 'abc';
+var b = 'bcd';
+// console.log(a<b) //it's return true
+
+var a = 'i am';
+var b = `"Shakil" Mahammud`;
+var c = a.concat(b)
+// console.log(c);
+// var d = c.substr(5,3);
+//  console.log(d);
+// console.log(c.charAt(5));
+// console.log(c.startsWith('i am'));
+// console.log(c.endsWith('Mahammud'));
+//console.log(a.toUpperCase());
+// console.log(b.toLowerCase());
+// console.log('  shakil  '.trim());
+// console.log(c.split(' '));
