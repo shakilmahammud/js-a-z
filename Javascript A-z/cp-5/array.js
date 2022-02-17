eb60f40b7cb349b518687fbd2de20e929cfc6afc
shakilmahammud <hshakil987@gmail.com>
2022-02-17 23:55:25 +0600

    array practices

--- Javascript A-z/cp-5/array.js
+++ Javascript A-z/cp-5/array.js
@@ -2,3 +2,11 @@
 // How to Declare A Array
 
 // let arr = [multiple value];
+
+// let arr = [1,2,3,4 , "hello", "hi"] // number and int array
+// console.log(arr);
+
+// let arr = new Array() //The syntax of creating array directly
+
+// arr[0] = "coder"
+// console.log(arr);
