b96f4ed188d04a8a674ace683e252eefce9e1e7f
shakilmahammud <hshakil987@gmail.com>
2022-02-18 23:55:47 +0600

    array entries method

--- Javascript A-z/cp-5/array.js
+++ Javascript A-z/cp-5/array.js
@@ -10,3 +10,22 @@
 
 // arr[0] = "coder"
 // console.log(arr);
+
+// let arr1 = [1,2,3,4,5]
+
+// let arr2 = [6,7,8,9,10]
+
+// let arrfinal = arr1.concat(arr2) // create a new array add broth array value in arrfinal
+
+// console.log(arrfinal)
+
+// var arr=["AngularJS","Node.js","JQuery","Bootstrap"]  
+// // place at 0th position, the element between 1st and 2nd position.  
+// var result=arr.copyWithin(1,2);  
+// console.log(result)
+
+var arr=['John','Michael','Embrose','Herry','Lewis'];  
+    var itr=arr.entries();  
+    for (let e of itr){
+        console.log(e)
+    }
