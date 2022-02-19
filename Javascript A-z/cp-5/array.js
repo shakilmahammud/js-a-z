44157a3d630173ec8c845b498316e4d32dac550d
shakilmahammud <hshakil987@gmail.com>
2022-02-19 23:39:13 +0600

    array flat

--- Javascript A-z/cp-5/array.js
+++ Javascript A-z/cp-5/array.js
@@ -24,8 +24,25 @@
 // var result=arr.copyWithin(1,2);  
 // console.log(result)
 
-var arr=['John','Michael','Embrose','Herry','Lewis'];  
-    var itr=arr.entries();  
-    for (let e of itr){
-        console.log(e)
-    }
+// var arr=['John','Michael','Embrose','Herry','Lewis'];  
+//     var itr=arr.entries();  entries return value aff a key value with array method
+//     for (let e of itr){
+//         console.log(e)
+//     }
+
+// var marks=[50,40,45,37,50];  
+  
+// function check(value)  
+// {  
+//   return value>30;  //return false, as marks[4]=20  
+// }  
+
+// console.log(marks.every(check))
+
+// var arr=['a','b',['c','d']]; //given 2D array  
+// var newArr=arr.flat(); //using flat() method 
+// console.log(newArr)
+
+// var arr1=['a','b','c','d'];  
+// var arr2=[1,2,3,4];  
+// var newArr=arr1.flatMap((arr1,index)=>[arr1,arr2[index]]); //It will map and flatten arr1 and arr2.  
