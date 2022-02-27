11fd04832d706788ef15f5beafb730e1eb3abcde
shakilmahammud <hshakil987@gmail.com>
2022-02-27 23:51:29 +0600

    object

---
+++ Javascript A-z/cp-6/object.js
@@ -0,0 +1,20 @@
+//object declearation
+
+// let obj ={} // object
+
+// obj.value = 10; // this line add  value name and set value 10
+
+// console.log(obj)
+
+// let obj = {
+//     a: 10,
+//     b: 20,
+// }
+// console.log(obj.a)// this return object a value
+// console.log(obj.a + obj.b) // this return object sum value
+
+// console.log(obj['a'])// this line use array notation
+
+//  var emp=new Object();   
+// emp.id=101;  
+// console.log(emp['id'])// this line use array notation
