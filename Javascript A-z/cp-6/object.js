a68f8a26865440f8afd884a10f01c2a27312723a
shakilmahammud <hshakil987@gmail.com>
2022-03-03 23:47:52 +0600

    up

--- Javascript A-z/cp-6/object.js
+++ Javascript A-z/cp-6/object.js
@@ -91,3 +91,25 @@
     
 //   Object.seal(object1);  
 //   console.log(Object.isSealed(object1));   
+
+// Object.setPrototypeOf(obj, prototype)  
+// const object1 = {  
+//   a: 'string',  
+//   b: 34,  
+//   c: true  
+// };  
+// const object2 = {  
+//   a: 'start',  
+//   b: 33,  
+//   c: false  
+// };  
+// console.log(Object.values(object1),  
+//             Object.values(object1));
+
+//  const obj = {};  
+// Object.preventExtensions(obj);  
+// obj.o = 3;  
+  
+// console.log(  
+//     obj.hasOwnProperty("o")  
+// );   
