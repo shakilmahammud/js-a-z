0c89290aa8a38b2cb405596623e0455272b063f4
shakilmahammud <hshakil987@gmail.com>
2022-03-09 23:53:14 +0600

    up

--- Javascript A-z/cp-1/hello.js
+++ Javascript A-z/cp-1/hello.js
@@ -1,3 +1,25 @@
+
+var name = 10;//keyword var
+let veri = 10;//let //global variable
+const vari = 50;//let
+// vari =100;
+// console.log(name);
+
+//variable declare 
+// keyword variablename
+
+// store korar jonno // = value
+
+// a variable declare to value assigned  keyword,variablename,=,value
+
+// const func=()=>{
+//  let veri = 100; // local variable
+
+// }
+
+
+
+
 console.log('Hello World')
 console.log('I Love Bangladesh')
 console.log(45)
