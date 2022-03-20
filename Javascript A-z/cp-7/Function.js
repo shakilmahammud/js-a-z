c772e315b57a3008528f62c5398d2ded6bab5466
shakilmahammud <hshakil987@gmail.com>
2022-03-20 23:51:21 +0600

    added practices

--- Javascript A-z/cp-7/Function.js
+++ Javascript A-z/cp-7/Function.js
@@ -15,3 +15,20 @@ function getInfo(){
 
     // var arr = [7, 5, 9, 1];  
 // var max = Math.max.apply(null, arr);  
+function msg(){  
+    alert("hello! this is message");  
+}
+
+function getcube(number){  
+    alert(number*number*number);  
+
+}
+
+function getInfo(){  
+    return "hello skill! How r u?";  
+    }  
+
+    // function.apply(thisArg, [array])  
+
+    // var arr = [7, 5, 9, 1];  
+// var max = Math.max.apply(null, arr);  
