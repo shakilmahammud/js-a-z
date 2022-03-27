3c16bf35e208949b62ec47668ff6ae11192c77a0
shakilmahammud <hshakil987@gmail.com>
2022-03-27 23:44:55 +0600

    operator update

--- Javascript A-z/cp-1/hello.js
+++ Javascript A-z/cp-1/hello.js
@@ -10,24 +10,24 @@ const vari = 50;//let
 
 // store korar jonno // = value
 
-// a variable declare to value assigned  keyword,variablename,=,value
+// // a variable declare to value assigned  keyword,variablename,=,value
 
-// const func=()=>{
-//  let veri = 100; // local variable
+// // const func=()=>{
+// //  let veri = 100; // local variable
 
-// }
-
-
+// // }
 
 
-console.log('Hello World')
-console.log('I Love Bangladesh')
-console.log(45)
-console.log(60.789)
 
-console.log('My Fav No: ' + 9)
-console.log(9 + 9)
-console.log('9' + 9)
+
+// console.log('Hello World')
+// console.log('I Love Bangladesh')
+// console.log(45)
+// console.log(60.789)
+
+// console.log('My Fav No: ' + 9)
+// console.log(9 + 9)
+// console.log('9' + 9)
 
 //variable write rules
  //#not use special characters
--- Javascript A-z/cp-1/operator.js
+++ Javascript A-z/cp-1/operator.js
@@ -68,3 +68,90 @@ console.log(c !== d)
 // >>
 
 console.log(typeof 'String')
+// Operator
+
+/*
+
+1. Arithmetic Operator
+2. comparison Operator
+3. Bitwise Operator
+4. Logical Operator
+5. Assignment operator
+6.Special Operator
+
+*/
+
+// Arithmetic Operator
+
+/* 
+1. + (Addition)
+2. - (Subtraction)
+3 . * (Multiplication)
+4 . / (Division)
+5. % (Modules)
+6 . ++ (Increment)
+7. -- (Decrement)
+*/
+
+//  10 + 25 
+// 25 - 10
+// 5*5 
+
+// 25/5 
+
+// 25%5
+
+// 5++,  6
+// 5--, 4
+
+// comparison Operator
+
+/*
+1. == (is equal to) 4=="4"
+2. === (is equal and same type) 4==="4"
+3. != (Not equal)  4 != 4
+4 . !== ( Not equal  same type)
+5 . > (Greater than)
+6. >= (Greater than Equal)
+7 . < (Less than)
+8 . <= (Less than Equal)
+
+*/
+
+// Bitwise Operator
+
+/*
+ 1. & (Bitwise AND) // (10==5 & 10==30) = false
+ 2. | (Bitwise OR) // (10==5 | 10==30) = false
+ 3. ^ (Bitwise XOR) // (10==5 ^ 10==30) = false
+ 4. ~ (Bitwise NOT) // (~5)
+ 5 . << (Bitwise Left Shift) // 00001 = 00010
+ 6 . >> (Bitwise Right Shift) // 000001
+ 7. >>> (Bitwise Right Shift With Zero)
+*/
+
+// Logical Operator
+
+/*
+1. && ( Logical And) // (10==5 && 10==30) 
+2.  || (logical OR) (10==5 || 10==30) 
+
+3. ! (Logical Not) // (10!=5)
+*/
+
+// Assignment operator
+
+/*
+1. = (Assign) a = 5 , b=a
+2. += (add & assign)// a+=5 // a=a+5, a=10
+3. -= (subtract)// a-=5 // a=a-5, a=0
+4. *= (Multiply and assign) // a*=5 // a = a*5 a= 25
+*/
+
+//Special Operator 
+
+/*
+1. typeof operator
+2 .void 
+3. new 
+*/
